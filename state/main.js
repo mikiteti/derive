4e342305bf486d2fe678ffcf08ab3ebeaaea7ddb
Téti Miklós <mikiteti@icloud.com>
2026-03-10 18:05:28 +0100

    welcome message bug fixed for now, will need fixing later

--- state/main.js
+++ state/main.js
@@ -57,13 +57,14 @@ class State {
 
         this.systemFiles = systemFiles;
         this.URL = new URL(window.location);
-        if (this.settings.welcomeMessage) {
-            this.URL.searchParams.delete("note");
-            history.replaceState({}, "", this.URL);
-        }
         this.note_url = this.URL.searchParams.get("note");
+        // if (this.settings.welcomeMessage) {
+        //     this.URL.searchParams.delete("note");
+        //     history.replaceState({}, "", this.URL);
+        // }
 
         this.initFuzzyFinders().then(() => {
+            console.log(this.note_url);
             if (this.note_url != undefined && this.files.find(e => e.url == this.note_url).content) {
                 console.log({ note_url: this.note_url });
                 console.log(this.files.find(e => e.url == this.note_url));
@@ -397,7 +398,8 @@ class State {
             if (file.id === undefined) file = await this.createFile(file);
             file = this.files.find(e => e.id === file.id)
         }
-        if (!this.settings.welcomeMessage) this.setNoteUrl(file.url);
+        // if (!this.settings.welcomeMessage) this.setNoteUrl(file.url);
+        this.setNoteUrl(file.url);
 
         let editor = this.editors.filter(e => e.fileId != undefined).find(e => e.fileId === file.id);
         if (editor == undefined) {
@@ -476,7 +478,10 @@ class State {
         }
 
         let currentFile;
-        if (this.note_url != undefined) currentFile = await this.getFile({ url: this.note_url });
+        if (this.note_url != undefined) {
+            console.log(this.note_url);
+            currentFile = await this.getFile({ url: this.note_url });
+        }
         if (currentFile != undefined && currentFile != -1) {
             (json.find(e => e.url == this.note_url) == undefined)
                 ? json.push(currentFile)
